a1fc8fdab95a818e0ed06fe18fc495b03ceddacd
mahesh123-8 <78149431+mahesh123-8@users.noreply.github.com>
2023-10-12 12:50:26 +0000

    initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId= 144553
+let accountEmail= "sutharmahesh9790@gmail.com"
+var accountPassword ="12345"
+accountCity="Jaipur"
+let accountState;
+
+
+//accountId = 2//not allowed
+accountEmail="hjsdw@gmail.com"
+accountPassword="23434"
+accountCity="mumbai"
+
+console.log(accountId);
+/* Prefer not to use  var 
+because  of issue in blockscop and functional scope*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+ "use strict";   // treat all JS code as newer version
+
+ //alert("2+3")//we are using nodejs,not browser
+
+ console.log(2+3);//code readabitlity should be high
+ console.log("Mahesh")
+
+ let name ="mahesh"
+ let age =29
+ let isLoggedIn=false
+ let state;
+//primitve //number => 2 to power 53
+            //bigInt
+       //string =>"" 
+           //boolean= t/f
+ //null=>standlone value
+ //undefined=>
+ //symbol=>uniqueness
+
+ //object
+  console.log(typeof null)//output object
+                //udefined //same 
